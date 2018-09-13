fd9fffdd9ec6b97233f1c1dfe501a24d881f138a
Francesca Ropolo <franci.ropolo@gmail.com>
2018-09-13 15:23:44 +0200

    created the events.js in data

---
+++ data/events.js
@@ -0,0 +1,17 @@
+const data = [{
+  name: 'Ironbeers',
+  type: 'Party',
+  date: '2018-09-14'
+}, {
+  name: 'BBQ',
+  type: 'Party',
+  date: '2018-08-31'
+}, {
+  name: 'Game Show',
+  type: 'Presentation',
+  date: '2018-09-07'
+}, {
+  name: 'UX-UI',
+  type: 'Talk',
+  date: '2018-09-12'
+}];
