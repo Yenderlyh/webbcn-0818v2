6bf73e2bfbb7cea3287cad661c2747757b9bff44
jsplmns <jonathan@spielmannsbilder.de>
2018-09-13 12:27:02 +0200

    first commit projects

--- routes/projects.js
+++ routes/projects.js
@@ -3,5 +3,82 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  const data = [{
+    projectName: 'Space Invaders',
+    studentName: 'Maria Jose',
+    presentationLink: 'https://slides.com/mjhr/space-invaders/#/',
+    projectLink: 'https://mjhrhacker.github.io/space-invaders/',
+    imageURL: 'https://media.giphy.com/media/YlTsQ2RTMaFpWF3It0/giphy.gif'
+  },
+
+  {
+    projectName: 'ROCK - PAPER - SCISSORS',
+    studentName: 'Diana Columbiana',
+    presentationLink: 'https://slides.com/dianablanco/deck/#/',
+    projectLink: 'https://dbpautt.github.io/Rock-paper-scissors/',
+    imageURL: 'https://media.giphy.com/media/2t9m7p4fY2DQEKT7fM/giphy.gif'
+  },
+
+  {
+    projectName: 'Franguinho',
+    studentName: 'Caroline',
+    presentationLink: 'https://docs.google.com/presentation/d/1jOY2LA9R1Gi95OuXFTCMzi3nCLeBuWuUaXtUCPO_lzc/edit#slide=id.p',
+    projectLink: 'https://caroline-gmr.github.io/franguinho/',
+    imageURL: 'https://media.giphy.com/media/9JegwSqBdhZPTWZTTU/giphy.gif'
+  },
+
+  {
+    projectName: 'Gay Heaven',
+    studentName: 'Barbara',
+    presentationLink: 'https://docs.google.com/presentation/d/1gNDbHGPemgxaYoJ2x4el1SlYgUC3Qwr-Gb6QCHLFcd8/edit',
+    projectLink: 'https://barkrrr.github.io/gay-heaven/',
+    imageURL: 'https://media.giphy.com/media/cRNb3CMoMWMh0BOvX2/giphy.gif'
+  },
+
+  {
+    projectName: 'Brick Breaker',
+    studentName: 'Jonathan',
+    presentationLink: 'https://slides.com/jonathanskudlik/deck#/',
+    projectLink: 'https://jsplmns.github.io/IRNHCK_m1-project-brick-breaker/',
+    imageURL: 'https://media.giphy.com/media/1wpxKB39vRX6xMaIY2/giphy.gif'
+  },
+
+  {
+    projectName: 'Brick Race',
+    studentName: 'Yenderly',
+    presentationLink: 'https://slides.com/yendy/my-name-is-yenderly-and-i-present-you-brick-race#/',
+    projectLink: 'https://slides.com/yendy/my-name-is-yenderly-and-i-present-you-brick-race#/',
+    imageURL: 'https://media.giphy.com/media/cYHxcdV6qYuAmEYA65/giphy.gif'
+  },
+
+  {
+    projectName: 'Pong Clone',
+    studentName: 'Gabriela',
+    presentationLink: 'https://docs.google.com/presentation/d/1UF--GqueyfpnKf6giEiNEeotXsQCD-MplbPjlLxGcoE/edit?usp=sharing',
+    projectLink: 'https://gap1885.github.io/Pong-clon-/',
+    imageURL: 'https://media.giphy.com/media/AgORULDNGG4L06s0qz/giphy.gif'
+  },
+
+  {
+    projectName: 'Web Dev life',
+    studentName: 'Francesca',
+    presentationLink: 'https://docs.google.com/presentation/d/1dgAmdQohaIMBzYpxkBA0fZXbUOYnzNlT29QZfzKbjzc/edit?usp=sharing',
+    projectLink: 'https://francescaropolo.github.io/webdev-life-game/',
+    imageURL: 'https://media.giphy.com/media/xV8c4uqtlfX5Z2SKKe/giphy.gif'
+  },
+
+  {
+    projectName: 'Underdog',
+    studentName: 'Axel',
+    presentationLink: 'https://slides.com/axelgarcia-1/project-1underdog#/',
+    projectLink: 'https://axelgar.github.io/underdog/',
+    imageURL: 'https://media.giphy.com/media/3aW3JtgEEYY6Hd5IhZ/giphy.gif'
+  }
+
+  ];
+
+  res.render('projects', { data });
+});
 
 module.exports = router;
