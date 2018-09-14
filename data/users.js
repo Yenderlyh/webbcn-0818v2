63e18042d37dbe399bd3f9fc202ca433783a4193
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2018-09-14 16:48:45 +0200

    (re)seed users

---
+++ data/users.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const users = [{
+  url: '/students/axel',
+  name: 'Axel',
+  country: 'España'
+}, {
+  url: '/students/barbara',
+  name: 'Barbara',
+  country: 'Deutschland'
+}, {
+  url: '/students/caroline',
+  name: 'Caroline',
+  country: 'Brasil'
+}, {
+  url: '/students/diana',
+  name: 'Diana',
+  country: 'Colombia'
+}, {
+  url: '/students/francesca',
+  name: 'Francesca',
+  country: 'Italia'
+}, {
+  url: '/students/gabriela',
+  name: 'Gabriela',
+  country: 'Ecuador'
+}, {
+  url: '/students/jonathan',
+  name: 'Jonathan',
+  country: 'Deutschland'
+}, {
+  url: '/students/maria-jose',
+  name: 'Maria José',
+  country: 'España'
+}, {
+  url: '/students/yenderly',
+  name: 'Yenderly',
+  country: 'Venezuela'
+}];
+
+module.exports = users;
